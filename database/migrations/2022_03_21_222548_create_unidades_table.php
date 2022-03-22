c24f817cfca72725c1297d387addf343f1c5d6ef
Buddy <buddy.m091@gmail.com>
2022-03-22 00:26:26 -0400

    Migracion de tablas del sistema antiguo

---
+++ database/migrations/2022_03_21_222548_create_unidades_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('direccion_id')->nullable()->constrained('direcciones');
+            $table->foreignId('office_id')->nullable()->constrained('offices');
+            $table->string('nombre')->nullable();
+            $table->string('sigla')->nullable();
+            $table->text('direccion')->nullable();
+            $table->smallInteger('estado')->nullable()->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidades');
+    }
+}
